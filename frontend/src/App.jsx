08c6dfc8b0a8ee559db17aa88acb3b3e6b90b16c
Affan-Codes <affan.khan4cs@gmail.com>
2025-07-10 20:25:54 +0530

    Footer section done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserLayout />} />
-        <Route />
+        <Route path="/" element={<UserLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+
+        <Route>{/* Admin Layout */}</Route>
       </Routes>
     </Router>
   );
