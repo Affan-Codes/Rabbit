12410766bdd750de5e5430849910a361169d9fa8
Affan-Codes <affan.khan4cs@gmail.com>
2025-07-19 13:12:07 +0530

    Add admin dashboard features including user and product management, order details, and order confirmation pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,13 @@ import Profile from "./pages/Profile";
 import CollectionPage from "./pages/CollectionPage";
 import ProductDetails from "./components/Products/ProductDetails";
 import Checkout from "./components/Cart/Checkout";
+import OrderConfirmationPage from "./pages/OrderConfirmationPage";
+import OrderDetailsPage from "./pages/OrderDetailsPage";
+import MyOrdersPage from "./pages/MyOrdersPage";
+import AdminLayout from "./components/Admin/AdminLayout";
+import AdminHomePage from "./pages/AdminHomePage";
+import UserManagement from "./components/Admin/UserManagement";
+import ProductManagement from "./components/Admin/ProductManagement";
 
 const App = () => {
   return (
@@ -23,9 +30,19 @@ const App = () => {
           <Route path="collections/:collection" element={<CollectionPage />} />
           <Route path="product/:id" element={<ProductDetails />} />
           <Route path="checkout" element={<Checkout />} />
+          <Route
+            path="order-confirmation"
+            element={<OrderConfirmationPage />}
+          />
+          <Route path="order/:id" element={<OrderDetailsPage />} />
+          <Route path="my-orders" element={<MyOrdersPage />} />
         </Route>
 
-        <Route>{/* Admin Layout */}</Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+          <Route path="users" element={<UserManagement />} />
+          <Route path="products" element={<ProductManagement />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ frontend/src/components/Admin/ProductManagement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductManagement = () => {
+  return (
+    <div>
+      manage
+    </div>
+  )
+}
+
+export default ProductManagement
